a360fadb726f1449049443ab9528a369c9178252
jackznq <zhangnaiquan@qjdchina.com>
2019-09-20 16:42:00 +0800

    commit

---
+++ common/src/main/java/com/smartcloud/common/utils/leetcode/Main.java
@@ -0,0 +1,28 @@
+package com.smartcloud.common.utils.leetcode;
+
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author andy
+ * @ClassName Main
+ * @date 2019-08-27 16:46
+ * @description todo
+ **/
+public class Main {
+    public static void main(String[] args) throws Throwable {
+
+        String data = "{\n" +
+                "    \"id\": 1337,\n" +
+                "    \"jsonrpc\": \"2.0\",\n" +
+                "    \"method\": \"getData\",\n" +
+                "    \"params\": []\n" +
+                "}";
+        JsonRpcHttpClient client = new JsonRpcHttpClient(
+                new URL("http://localhost:8091/contract/template?"+data));
+        Integer getData = client.invoke("getData", null, Integer.class);
+        System.out.println(getData);
+    }
+}
